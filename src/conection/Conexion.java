9e028af0761d84a861c73ebc8e7aa7826e401228
Nimrod Holguin <nimrodholguin@gmail.com>
2019-03-18 12:44:23 -0400

    conexion controlada

--- src/conection/Conexion.java
+++ src/conection/Conexion.java
@@ -1,8 +1,14 @@
 
 package conection;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -15,15 +21,45 @@ import org.apache.commons.dbcp2.BasicDataSource;
 public class Conexion {
     
     private static BasicDataSource dataSource = null;
+    private static String url;
+    private static String puerto;
+    private static String username;
+    private static String password;
+    private static String serverTimezone; //America/Caracas --- Default
+    
+    private static void initValues() {
+        
+        try {
+            FileReader entrada = new FileReader("txt/conexion.txt");
+            BufferedReader br = new BufferedReader(entrada);
+            
+            try {
+                url = br.readLine();
+                puerto = br.readLine();
+                username = br.readLine();
+                password = br.readLine();
+                serverTimezone = br.readLine();
+            } catch (IOException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
     
     private static DataSource getDataSource() {
+        
+        initValues();
+        
         if(dataSource == null) {
             dataSource = new BasicDataSource();
             dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-            dataSource.setUsername("root");
-            dataSource.setPassword("MyPass1001");
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
             //Correcion de Timezone
-            dataSource.setUrl("jdbc:mysql://localhost:3306/mail?user=me&pass=secret&useLegacyDatetimeCode=false&serverTimezone=America/Caracas");
+            dataSource.setUrl("jdbc:mysql://"+url+":"+puerto+"/mail?user=me&pass=secret&useLegacyDatetimeCode=false&serverTimezone="+serverTimezone);
             dataSource.setInitialSize(20);
             dataSource.setMaxIdle(15);
             dataSource.setMaxTotal(20);
